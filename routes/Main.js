2f7c41885dd87927138febfba4cd5d3dae7ecea9
Anthony Bruscantini <ajbruscantini@gmail.com>
2017-02-21 14:29:09 +0100

    Added and modified routes in Main.js

--- routes/Main.js
+++ routes/Main.js
@@ -7,7 +7,7 @@ const Trade       = require('../models/trade');
 const passport    = require("passport");
 const ensureLogin = require("connect-ensure-login");
 
-//router.use(ensureLogin.ensureLoggedIn('/login'));
+router.use(ensureLogin.ensureLoggedIn('/login'));
 
 router.get('/home', (req, res, next) => {
   Item.find((err, items) => {
@@ -21,7 +21,21 @@ router.get('/home', (req, res, next) => {
 });
 
 router.get('/item/:id', (req, res, next) => {
-  res.render('item', {layout: "layouts/home-layout"});
+  const itemId = req.params.id;
+  Item.findById(itemId, (err, item) => {
+    if (err) return next(err);
+    return res.render('item', {layout: "layouts/home-layout",
+                                item, user});
+  });
+
+});
+
+router.get('/add', (req, res, next) => {
+
+});
+
+router.post('/add', (req, res, next) => {
+
 });
 
 module.exports = router;
